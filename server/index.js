82a5b1e4b33570c3694a2a9224d6e32f96515062
Gideon Cohen <gideonacohen@gmail.com>
2026-03-29 16:48:16 -0400

    Trust proxy for Railway deployment

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const server = http.createServer(app);
 
 // --- Middleware ---
 
+app.set('trust proxy', 1); // trust Railway's proxy for secure cookies
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
